c54f02509c35e2b94f98c14f17c41d32747c41c1
Rob Squires <rob.squires@ft.com>
2018-11-04 18:02:55 +0000

    save tweet to s3

--- handler.js
+++ handler.js
@@ -2,16 +2,24 @@
 
 const ingestTweet = require('./lib/ingest-tweet');
 
-module.exports.processTweet = (event, context, callback) => {
-	const body = JSON.parse(event.body);
-	const data = ingestTweet(body.tweet);
-	console.log(data)
-	callback(null, {
-		statusCode: 200,
+function writeResponse (statusCode, data) {
+	return {
+		statusCode,
 		headers: {
 			'content-type': 'application/json',
 			'Cache-Control': 'max-age=0, no-cache, no-store, must-revalidate'
 		},
 		body: JSON.stringify(data)
-	});
+	}
+}
+
+module.exports.ingestTweet = (event, context, callback) => {
+	const body = JSON.parse(event.body)
+	ingestTweet(body.tweet).then(data => {
+		callback(null, writeResponse(200, data));
+	})
+	.catch(err => {
+		console.error(err);
+		callback(null, writeResponse(500, err));
+	})
 }
