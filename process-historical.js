356a7d2e5b979e285def817ebf1b37a60a33d7c9
Rob Squires <rob.squires@ft.com>
2020-10-02 20:57:15 +0100

    process tweets from 2020

--- process-historical.js
+++ process-historical.js
@@ -1,10 +1,11 @@
 const { getTemperature } = require('./lib/steps')
-const rawData = require('./data/historical-tweets.json')
+const rawData = require('./data/brockwell-lido-historical-2020.json')
 const fs = require('fs')
 
 const processedData = rawData
 	.map(({ text, date }) => ({ date, temperature: getTemperature(text)}))
 	.filter(({ temperature }) => !!temperature)
 
+console.log("Number tweets read:", rawData.length, "Number successfully parsed:", processedData.length);
 
-	fs.writeFileSync('./brockwell-lido-historical.json', JSON.stringify(processedData, undefined, "\t"));
+fs.writeFileSync('./brockwell-lido-historical.json', JSON.stringify(processedData, undefined));
